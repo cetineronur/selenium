9a94216c854502568743f9debd321b1b6328ee4c
onur cetiner <cetineronur44@gmail.com>
2022-01-31 21:06:34 +0100

    neue Classes wurde eingefugt

--- com.TestNGBatch44/src/test/java/tests/day13/AmazonTest.java
+++ com.TestNGBatch44/src/test/java/tests/day13/AmazonTest.java
@@ -8,46 +8,26 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utilities.TestBase;
+
+import java.util.List;
 
 public class AmazonTest extends TestBase {
 
-    /*
-    facebook hesap olusturma sayfasi testi
-    https://www.facebook.com/ sayfasindan >>>Yeni hesap olustur sayfasina gidin
-     isim girisi yapin
-     soy isim girisi yapin
-     cep numarasi +38669645099 girin
-     en az 6 rakam bir harf ve noktalama isareti olan sifre girisi yapin
-     Dogum tarihi 18 yasindan kucuk bir tarih secin
-     cinsiyet secimi yapin farketmez
-     kaydol a tiklayin
-     beklenen sonuc 18 yas alti uyarisi vermesi
-     */
 
     @Test
-    public void warning_letter() throws InterruptedException {
-       driver.get("https://de-de.facebook.com/");
-      driver.findElement(By.xpath("(//button[@value='1'])[3]")).click();
-        Actions actions = new Actions(driver);
-        Thread.sleep(3000);
-        driver.findElement(By.xpath("(//a[@role='button'])[2]")).click();
-       WebElement firstname=driver.findElement(By.xpath("//input[@name='firstname']"));
-       // Actions actions1 = new Actions(driver);
-       actions.sendKeys("Onur").sendKeys(Keys.TAB).sendKeys("Cetiner").
-               sendKeys(Keys.TAB).sendKeys("+38669645099").sendKeys(Keys.TAB).sendKeys("123456a.").
-               sendKeys(Keys.TAB).perform();
+    public void test() throws InterruptedException {
 
-       WebElement birthday= driver.findElement(By.xpath("//select[@id='day']"));
-        Select select = new Select(birthday);
-        select.selectByValue("21");
-        actions.sendKeys(Keys.TAB).sendKeys("Kas").
-        sendKeys(Keys.TAB).sendKeys("2009").
-                sendKeys(Keys.TAB).sendKeys(Keys.TAB).perform();
-        driver.findElement(By.xpath("(//input[@type='radio'])[2]")).click();
-        driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
-        Thread.sleep(5000);
+        driver.navigate().to("https://facebook.com");
+        driver.findElement(By.xpath("//button[@data-testid='cookie-policy-dialog-accept-button']")).click();
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        WebElement engElementi = driver.findElement(By.xpath("//a[@href='https://en-gb.facebook.com/']"));
+        engElementi.click();
+
+
+        Thread.sleep(3000);
 
     }
-
 }
---
+++ com.TestNGBatch44/src/test/java/tests/pracice/P1.java
@@ -0,0 +1,18 @@
+package tests.pracice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class P1 extends TestBase {
+
+    @Test
+    public void test(){
+
+
+    }
+}
